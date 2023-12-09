275ed34f5a85140e8e46b38ffe3b22f241fe95ac
Jayden Rodriguez <02rodriguez.j@gmail.com>
2023-12-08 23:33:25 -0700

    Create task /get and /post in model/router

--- api/task/model.js
+++ api/task/model.js
@@ -1,1 +1,73 @@
 // build your `Task` model here
+const db = require('../../data/dbConfig')
+
+const task_completed_to_binary = (array) => {
+    const result = array.map(task => {
+        if (task.task_completed && task.task_completed !== null || task.project_completed === true){
+            task.task_completed = 1
+        } else if (!task.task_completed || task.task_completed === null || false) {
+            task.task_completed = 0
+        }
+        return task
+    })
+    return result
+}
+
+const task_completed_to_boolean = (array) => {
+    const result = array.map(task => {
+        if (task.task_completed && task.task_completed !== null || task.project_completed === 1){
+            task.task_completed = true
+        } else if (!task.task_completed || task.task_completed === null || task.task_completed === 0) {
+            task.task_completed = false
+        }
+        return task
+    })
+    return result
+}
+
+const getAll = async () => {
+    const tasks = await db('tasks as ts')
+    .select('task_id', 'task_description', 'task_notes', 'task_completed', 'project_name', 'project_description')
+        .join('projects as p', 'p.project_id', '=', 'ts.project_id')
+
+    const formatted_tasks = task_completed_to_boolean(tasks)
+
+    return formatted_tasks
+}
+
+const create = async (body) => {
+    if (!body.task_description){
+        return {undefined: true, message: 'Must include task_description!'}
+    } else if (!body.project_id){
+        return {undefined: true, message: 'Must include project_id!'}
+    }
+
+    const does_project_exist = await db('projects').where('project_id', body.project_id)
+
+    if (!does_project_exist) {
+        return {undefined: true, message: 'Invalid project_id!'}
+    }
+
+    const body_to_post = {
+        task_description: body.task_description,
+        task_notes: body.task_notes || null,
+        task_completed: body.task_completed || null,
+        project_id: body.project_id
+    }
+
+    const [formatted_body_to_post] = task_completed_to_binary([body_to_post])
+
+    const [id] = await db('tasks').insert(formatted_body_to_post)
+
+    const new_task = await db('tasks').where('task_id', id)
+
+    const [formatted_new_task] = task_completed_to_boolean(new_task)
+
+    return formatted_new_task
+}
+
+
+module.exports = {
+    getAll,
+    create
+}
--- api/task/router.js
+++ api/task/router.js
@@ -1,23 +1,35 @@
 // build your `/api/tasks` router here
 const express = require('express')
+const Task_Model = require('./model')
 const task = express.Router()
 
 task.get('/', async (req, res, next) => {
     try {
-        res.status(200).json({
-            message: 'dog'
-        })
+        const tasks = await Task_Model.getAll()
+        res.status(200).json(tasks)
     } catch(err) {
-        err.status = 500
-        err.message = `Error in fetching resources`
-        next(err)
+        res.status(500).json({
+            message: "Error in getting tasks: " + err.message
+        })
     }
 })
 
-task.use((err, req, res, next) => {
-    res.status(err.status || 500).json({
-        message: err.message
-    })
+task.post('/', async (req, res, next) => {
+    try {
+        const new_task = await Task_Model.create(req.body)
+        if (new_task?.undefined !== undefined){
+            res.status(400).json({
+                message: `${new_task.message}`
+            })
+        } else {
+            res.status(201).json(new_task)
+        }
+    } catch(err){
+        res.status(500).json({
+            message: "Error in creating new post: " + err.message
+        })
+    }
 })
+
 
 module.exports = task
